92af7db7cd9d8d859293b0acc378ef9bad00e10c
SUTAPA GHOSH <sutapaghosh0405@gmail.com>
2025-09-09 20:13:47 +0530

    Removed node_modules and updated .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+function App() {
+  const [color,setColor]=useState("white")
+  return (
+    <>
+    <div className="h-screen w-screen flex justify-center items-center gap-4" style={{backgroundColor:color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg px-2 py-3 rounded-3xl bg-green-200">
+          <button onClick={()=> setColor("white")} className="outline-none px-4 py-1 rounded-full text-black shadow-lg" style={{backgroundColor:"white"}}>White</button>
+          <button onClick={()=> setColor("violet")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"violet"}}>Violet</button>
+          <button onClick={()=> setColor("blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"blue"}}>Blue</button>
+          <button onClick={()=> setColor("skyblue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"skyblue"}}>Sky</button>
+          <button onClick={()=> setColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"green"}}>Green</button>
+          <button onClick={()=> setColor("yellow")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"yellow"}}>Yellow</button>
+          <button onClick={()=> setColor("orange")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"orange"}}>Orange</button>
+          <button onClick={()=> setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+          <button onClick={()=> setColor("black")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"black"}}>Black</button>
+
+        </div>
+      </div>
+     </div>
+    </>
+  )
+}
+export default App
